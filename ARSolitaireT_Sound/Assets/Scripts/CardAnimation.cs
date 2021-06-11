5c62213d498930f910d889e8d76c7948cd1cad8d
Yim_daun <cactus040919@dgsw.hs.kr>
2021-06-11 16:42:28 +0900

    2021.06.11
    카드 클릭 소리만 성공

---
+++ ARSolitaireT_Sound/Assets/Scripts/CardAnimation.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardAnimation : MonoBehaviour
+{
+	private float time = 0.5f;
+	private Vector3 cardSet;
+
+	void Start()
+	{
+		cardSet = new Vector3(0.0f, -0.5f, -0.2f);
+	}
+
+	public void MovingAnimation(GameObject card, Transform destination)
+	{
+		LeanTween.move(card, destination.position + cardSet, time);
+		Debug.Log(time);
+		Debug.Log(time);
+		card.transform.parent = destination;
+	}
+}
