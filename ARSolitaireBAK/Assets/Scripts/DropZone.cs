4f30e6141c23692af108702661d424f609da625a
wjqrltls <17431743AA@gmail.com>
2021-06-16 18:04:40 +0900

    2021.06.16
    애니메이션 오류 수정

---
+++ ARSolitaireBAK/Assets/Scripts/DropZone.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
+{
+
+	public void OnPointerEnter(PointerEventData eventData)
+	{ //영역 위에 마우스커서가  있다
+		Debug.Log("OnPointerEnter");
+		if (eventData.pointerDrag == null)
+			return;
+		Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+		if (d != null)
+		{
+			d.placeholderParent = this.transform;
+		}
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{ // 영역 밖으로 마우스커서 있다
+		Debug.Log("OnPointerExit");
+		if (eventData.pointerDrag == null)
+			return;
+
+		Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+		if (d != null && d.placeholderParent == this.transform)
+		{
+			d.placeholderParent = d.parentToReturnTo;
+		}
+	}
+
+	public void OnDrop(PointerEventData eventData)
+	{ // 놓기
+		Debug.Log(eventData.pointerDrag.name + " was dropped on " + gameObject.name);
+
+		Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+		if (d != null)
+		{
+			d.parentToReturnTo = this.transform;
+		}
+
+	}
+}
---
+++ ARSolitaireBAK/Assets/Scripts/SoundMgr.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundMgr : MonoBehaviour
+{
+    public static SoundMgr Intance = null;
+
+    public AudioClip cardswap;
+    public AudioClip cardclick;
+
+    public float LowPitchRange = 0.95f;
+    public float HighPitchRange = 1.05f;
+
+    public AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(Intance == null)
+        {
+            Intance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+        
+    }
+
+    public void CardSwap()
+    {
+        audioSource.clip = cardswap;
+        audioSource.Play();
+    }
+
+    public void CardClick()
+    {
+        audioSource.clip = cardclick;
+        audioSource.Play();
+    }
+}
